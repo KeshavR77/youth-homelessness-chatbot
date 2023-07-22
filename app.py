cabc77ae5604091c82536633adb2d0c6dcc03157
keshavrangan <keshavrangan2007@gmail.com>
2023-07-21 18:53:40 -0700

    change of app

--- app.py
+++ app.py
@@ -1,3 +1,39 @@
+import openai
 import streamlit as st
 
 st.write("hello world")
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+def call_chatgpt(prompt: str) -> str:
+    """
+    Uses the OpenAI API to generate an AI response to a prompt.
+
+    Args:
+        prompt: A string representing the prompt to send to the OpenAI API.
+
+    Returns:
+        A string representing the AI's generated response.
+
+    """
+
+    # Use the OpenAI API to generate a response based on the input prompt.
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=500,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+
+    # Extract the text from the first (and only) choice in the response output.
+    ans = response.choices[0]["text"]
+
+    # Return the generated AI response.
+    return ans
+
+question = st.text_input("Input a question", "Tell me a joke")
+answer = call_chatgpt(question)
+st.write("answer")
